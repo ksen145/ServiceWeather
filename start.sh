c802a5e4ab3c38240ae78038fa5307e8e73ec9ef
Egor Safonov <ksen10405@gmail.com>
2023-10-15 08:08:16 +0300

    added requirements.txt

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 export TG_KEY=''
 export WE_KEY=''
 export GEO_KEY=''
-(trap 'kill 0' SIGINT; python3 test_site/manage.py runserver & python3 telegramm/bot.py)
+(trap 'kill 0' SIGINT; python3 telegramm/bot.py & python3 test_site/manage.py runserver)
